c7f13f496e9912d71175acff2704fede875b42ab
Rohitraj1f <121429694+Rohitraj1f@users.noreply.github.com>
2022-12-29 13:07:33 +0530

    Update in Odd.java Add sum method

--- Odd.java
+++ Odd.java
@@ -1,12 +1,21 @@
+class Sum{
+  public void OddSum(){
+    int s=0;
+  for(int i=0;i<10;i++){
+      s=s+i;
+     }
+    System.out.print("Sum of even no. is "+s);
+    }      
+}
 public class Odd {
     public static void main(String args[]){
         int sum=0;
         for(int i=1;i<=10;i++){
          if(i%2!=0){
-            sum=sum+i;
              System.out.println(i+" ");
          }
         }
-        System.out.println("Sum of odd no. is "+sum);
+        Sum s=new Sum();
+        s.OddSum();
      }
 }

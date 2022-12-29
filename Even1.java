35ed40be15d3ddf5fbef010f6f156bc64b7fc67a
Rohit Raj <rohitraj1f@gmail.com>
2022-12-29 11:27:06 +0530

    edit print line Signed-off-by: Rohit Raj <rohitraj1f@gmail.com>

--- Even1.java
+++ Even1.java
@@ -2,7 +2,7 @@ public class Even1 {
     public static void main(String args[]){
        for(int i=0;i<10;i++){
         if(i%2==0){
-            System.out.print("Even No."+i+" ");
+            System.out.println(i+" ");
         }
        }
     }
